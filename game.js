8d886945f5efff6968373e52bfa88ae4559faeb4
DougP723 <dougpeterson23@gmail.com>
2016-06-01 10:13:58 -0700

    test level

--- game.js
+++ game.js
@@ -1,25 +1,25 @@
 var gameport = document.getElementById("gameport");
 
-var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x3344ee});
+var renderer = PIXI.autoDetectRenderer(600, 600, {backgroundColor: 0x3344ee});
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
 
-var texture = PIXI.Texture.fromImage("cs413.png");
+var texture = PIXI.Texture.fromImage("main_character1.png");
 
 var sprite = new PIXI.Sprite(texture);
 
 sprite.anchor.x = 0.5;
 sprite.anchor.y = 0.5;
-
 sprite.position.x = 200;
 sprite.position.y = 200;
+sprite.vy = 1;
 
 stage.addChild(sprite);
 
 function animate() {
 	requestAnimationFrame(animate);
-	sprite.rotation += 0.1;
 	renderer.render(stage);
+	sprite.position.y += sprite.vy;
 }
 animate();

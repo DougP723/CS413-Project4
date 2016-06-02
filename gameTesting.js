8ad389c7d5d3c55797b4daf24758cf9dd1b5f5c0
DougP723 <dougpeterson23@gmail.com>
2016-06-02 15:30:33 -0700

    character animation is finished
    now the HUD wont appear to show player health. Going back to fix that

--- gameTesting.js
+++ gameTesting.js
@@ -13,7 +13,13 @@ stage.scale.y = game_scale;
 var GRAVITY = 10;
 var JUMP = -10;
 
-var player;
+//Player elements
+var player = new PIXI.Container();
+var standingSprite;
+var walkingSprite;
+var frames = [];
+
+//World elements
 var exit_object;
 var levelExit;
 var world;
@@ -27,20 +33,12 @@ var numPlatforms = 2; //This will change depending on the level
 var ground;
 var hero;
 var text;
-var enemy_object;
-var enemy;
 
-//HUD elements
-var HUD = new PIXI.Container();
-HUD.x = 0;
-HUD.y = 0;
-HUD.interactive = true;
-stage.addChild(HUD);
-var heart1;
-var heart2;
-var heart3;
-var heartCount = 3;
-var isHit = false;
+//Enemy elements
+var numEnemies = 2;
+var enemy_object;
+var enemies = [];
+var enemy;
 
 //Game Over Screen elements
 var gameOverScreen;
@@ -64,6 +62,18 @@ var levelMenu;
 var gameStart = false;
 var L1Button;
 
+//HUD elements
+var HUD = new PIXI.Container();
+HUD.x = 0;
+HUD.y = 0;
+HUD.interactive = true;
+stage.addChild(HUD);
+var heart1;
+var heart2;
+var heart3;
+var heartCount = 3;
+var isHit = false;
+
 //Collision detection code
 isIntersecting = function(r1, r2) {
         return !(r2.x-4 > (r1.x-4 + r1.width-4)  || 
@@ -76,15 +86,56 @@ function adjustCamera(){
 
 }
 
+changeAnimation = function(direction, state){
+	if (direction == "left" && state == "on"){
+		walkingSprite.play();
+		if (walkingSprite.scale.x != -1){
+			walkingSprite.scale.x = -1;
+			walkingSprite.anchor.x = 1;
+		}
+		player.addChild(walkingSprite);
+		player.removeChild(standingSprite);
+	}
+	if (direction == "right" && state == "on"){
+		walkingSprite.play();
+		if (walkingSprite.scale.x != 1){
+			walkingSprite.scale.x = 1;
+			walkingSprite.anchor.x = 0;
+		}
+		player.addChild(walkingSprite);
+		player.removeChild(standingSprite);
+	}
+	if (direction == "left" && state == "off"){
+		walkingSprite.stop();
+		if (standingSprite.scale.x != -1){
+			standingSprite.scale.x = -1;
+			standingSprite.anchor.x = 1;
+		}
+		player.addChild(standingSprite);
+		player.removeChild(walkingSprite);
+	}
+	if (direction == "right" && state == "off"){
+		walkingSprite.stop();
+		if (standingSprite.scale.x != 1){
+			standingSprite.scale.x = 1;
+			standingSprite.anchor.x = 0;
+		}
+		player.addChild(standingSprite);
+		player.removeChild(walkingSprite);
+	}
+}
+
 window.addEventListener("keydown", function (e) {
   e.preventDefault();
 
   	if (gO_boolean == false){
   		if (e.keyCode == 65){//This is the A key
   			player.vx = -4;
+  			changeAnimation("left", "on");
   		}
 		else if (e.keyCode == 68){//This is the D key
 			player.vx = 4;
+			changeAnimation("right", "on");
 		}
 		else if (e.keyCode == 32){//This is the SPACE key
 			jump();
@@ -98,9 +149,11 @@ window.addEventListener("keyup", function (e) {
 
   	if (e.keyCode == 65){//This is the A key
   		player.vx = 0;
+  		changeAnimation("left", "off");
   	}
 	else if (e.keyCode == 68){//This is the D key
 		player.vx = 0;
+		changeAnimation("right", "off");
 	}	
 	console.log(e.keyCode);
 	
@@ -151,26 +204,31 @@ function move(){
 }
 
 function checkDamage(){
-	if (isIntersecting(player, enemy)){
-		player.x = player.x - 15;
-		player.y = player.y - 15;
-		var new_x = player.x - 100;
-		var new_y = player.y - 25;
-		createjs.Tween.get(player.position).to({x: new_x, y: new_y}, 100, createjs.Ease.bounceOut);
-		if (heartCount == 3){
-			heart3.alpha = 0;
+
+	if (gameStart == true){
+		for (var i in enemies){
+			enemy = enemies[i];
+			if (isIntersecting(player, enemy)){
+				player.x = player.x - 15;
+				player.y = player.y - 15;
+				var new_x = player.x - 100;
+				var new_y = player.y - 25;
+				createjs.Tween.get(player.position).to({x: new_x, y: new_y}, 100, createjs.Ease.bounceOut);
+				if (heartCount == 3){
+					heart3.alpha = 0;
+				}
+				else if (heartCount == 2){
+					heart2.alpha = 0;
+				}
+				else if (heartCount == 1){
+					heart1.alpha = 0;
+					gO_boolean = true;
+					new_x = player.x - 500;
+					new_y = player.y + 1000;
+				}
+				heartCount--;
+			}
 		}
-		else if (heartCount == 2){
-			heart2.alpha = 0;
-		}
-		else if (heartCount == 1){
-			heart1.alpha = 0;
-			gO_boolean = true;
-			new_x = player.x - 500;
-			new_y = player.y + 1000;
-			createjs.Tween.get(player.position).to({x: new_x, y: new_y}, 5000);
-		}
-		heartCount--;
 	}
 }
 function quitGame(e){
@@ -202,7 +260,6 @@ checkFalling = function(verticalForce){
 		if (verticalForce >= 0){
 			if (isIntersecting(player, ground)){
 				player.vy = 0;
-				text.text = "array #" + j;
 				return;
 			}
 			else{
@@ -236,14 +293,20 @@ PIXI.loader
 	.add("backButton", "backbutton.png")
 	.add("gOScreen", "gameover_screen.png")
 	.add("levelWin", "levelWinScreen.png")
+	.add("amishwalk", "amishwalk.json")
 	.load(ready);
 
 function ready(){
-	
-	text = new PIXI.Text('',{font : '40px Arial', fill : 0x000000, align : 'center'});
-	text.x = 50;
-	text.y = 100;
-	HUD.addChild(text);
+
+	for (var i = 1; i <= 26; i++){
+		frames.push(PIXI.Texture.fromFrame("main_character" + i + ".png"))
+	}
+
+	walkingSprite = new PIXI.extras.MovieClip(frames);
+	walkingSprite.animationSpeed = 1;
+	walkingSprite.x = 0;
+	walkingSprite.y = 0;
+	walkingSprite.play();
 	
 	//Main Menu Screen initialization
 	mainMenu = new PIXI.Sprite(PIXI.loader.resources.mainMenu.texture);
@@ -315,13 +378,14 @@ function ready(){
 
 function levelOne(){
 
+	player.removeChild(standingSprite);
 	//Initialize game parameters
 	heartCount = 3;
 	gO_boolean = false;
 	stage.x = -100;
 	stage.y = -1700;
-	stage.removeChild(HUD);
 	gameStart = true;
+
 	//World initialization
 	let tu = new TileUtilities(PIXI);
 	world = tu.makeTiledWorld("map_json", "tileset1.png");
@@ -334,30 +398,33 @@ function levelOne(){
 
 	//Barrier initialization
 	for (var k = 1; k <= numBarriers; k++){
-		barriers.push(world.getObject("barrier" + k))
+		barriers.push(world.getObject("barrier" + k));
 	}
 
 	//Player initialization
 	hero = world.getObject("hero");
-	player = new PIXI.Sprite(PIXI.loader.resources.player.texture);
+	standingSprite = new PIXI.Sprite(PIXI.loader.resources.player.texture);
 	player.x = hero.x;
 	player.y = hero.y;
 	player.width = 60;
 	player.height = 116;
-	player.anchor.x = 0.0;
-	player.anchor.y = 0.0;
 	player.vy = GRAVITY;
 	player.vx = 0;
+	player.addChild(standingSprite);
+
 
 	//Enemy initialization
-	enemy_object = world.getObject("enemy");
-	enemy = new PIXI.Sprite(PIXI.loader.resources.enemy.texture);
-	enemy.x = enemy_object.x;
-	enemy.y = enemy_object.y;
-	enemy.width = 50;
-	enemy.height = 50;
-	enemy.anchor.x = 0.0;
-	enemy.anchor.y = 0.0;
+	for (var j = 1; j <= numEnemies; j++){
+		enemies.push(world.getObject("enemy" + j));
+	}
+
+	//enemy = new PIXI.Sprite(PIXI.loader.resources.enemy.texture);
+	//enemy.x = enemy_object.x;
+	//enemy.y = enemy_object.y;
+	//enemy.width = 50;
+	//enemy.height = 50;
+	//enemy.anchor.x = 0.0;
+	//enemy.anchor.y = 0.0;
 
 	//Level Exit initialization
 	exit_object = world.getObject("levelExit");
@@ -389,13 +456,13 @@ function levelOne(){
 	ground = platforms[0];
 	var entity_layer = world.getObject("player");
 	entity_layer.addChild(levelExit);
+	entity_layer.addChild(walkingSprite);
 	entity_layer.addChild(player);
 	entity_layer.addChild(enemy);
 
 
-	HUD.x = -stage.x;
-	HUD.y = -stage.y;
-	stage.addChild(HUD);
+	//HUD.x = -stage.x;
+	//HUD.y = -stage.y;
 
 }
 
